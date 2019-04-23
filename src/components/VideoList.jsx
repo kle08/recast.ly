175b6dfc3b133dab5d42a82f5c369afd10ca727d
eugenia ong <eugenia.osy@gmail.com>
2019-04-22 20:40:53 -0700

    Refactor App to ES6 and hook video list and video player component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -2,8 +2,8 @@ import VideoListEntry from "./VideoListEntry.js";
 
 var VideoList = (props) => (
   <div className="video-list">
-    {props.videos.map(video =>
-      <VideoListEntry video={video} />
+    {props.videos.map((video, index) =>
+      <VideoListEntry video={video} whenClick={props.whenClick} index={index} />
     )}
   </div>
 );
