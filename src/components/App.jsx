d391f3f5eb3b2dac6123577d670504b4fe65291e
kle08 <kevinle1991@gmail.com>
2019-04-23 14:36:09 -0700

    Implement youtube search and linked to search button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
+import searchYouTube from "../lib/searchYouTube.js";
+import Search from "./Search.js";
 import VideoList from "./VideoList.js";
 import VideoPlayer from "./VideoPlayer.js";
 import exampleVideoData from "../data/exampleVideoData.js";
+import YOUTUBE_API_KEY from "../config/youtube.js";
 
 class App extends React.Component {
   constructor(props) {
@@ -8,17 +11,38 @@ class App extends React.Component {
 
     this.state = {
       currentVideo: exampleVideoData[0],
-      videos: exampleVideoData
+      videos: exampleVideoData,
+      query: 'react',
     };
 
     this.whenClick = this.whenClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentWillMount() {
+
+  }
+  
   whenClick(selectedVideo) {
     this.setState({
       currentVideo: selectedVideo
     });
+  }
 
+  handleChange(event) {
+    this.setState({
+      query: event.target.value
+    });
+  }
+
+  handleSubmit() {
+    var options = {
+      key: YOUTUBE_API_KEY,
+      query: this.state.query,
+      max: 5
+    }
+    searchYouTube(options, (data) => console.log(data));
   }
 
   render() {
@@ -26,7 +50,7 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <div><Search handleChange={this.handleChange} handleSubmit={this.handleSubmit} value={this.state.query}/></div>
           </div>
         </nav>
         <div className="row">
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,7 +1,31 @@
-import YOUTUBE_API_KEY from "../config/youtube.js";
 
 var searchYouTube = (options, callback) => {
-  // TODO
+  var url = "https://www.googleapis.com/youtube/v3/search";
+  var data = {
+    part: 'snippet',
+    type: 'video',
+    key: options.key,
+    q: options.query,
+    maxResults: options.max,
+    videoEmbeddable: true
+  };
+
+
+  $.ajax({
+    url: url,
+    type: 'GET',
+    contentType: 'application/json',
+    data: data,
+    // success: function(response) {
+    //   //console.log(response.items);
+    // },
+    error: function (data) {
+      console.error('Failed to fetch results', error);
+    }
+  }).done(function(response) {
+    callback(response.items);
+  });
+
 };
 
 export default searchYouTube;
